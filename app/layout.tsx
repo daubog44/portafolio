6b3519e048f4b5a16bbfa6fc5f9c6e49f56995ef
daubog44 <uopilul@gmail.com>
2022-12-14 15:45:19 +0100

    add title

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Next.js</title>
+        <title>Darius-portafolio</title>
       </head>
       <body>
         {children}
